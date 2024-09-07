b022999785e689af0cf6083d09630f4300c6f811
emiliano0807 <emilianorivfacio@gmail.com>
2024-09-06 20:04:42 -0600

    Codigo18

---
+++ Codigo18.c
@@ -0,0 +1,40 @@
+/*Simular una Tienda con entradas y salidas de capital con sentencias switch y if - else*/
+
+#include <stdio.h>
+int main(){
+	int opcion;
+	float agregar,retirar,saldo=3000;
+	printf("\t*****************************************\n");
+	printf("\t*Este es un Simulador de Tienda o Cajero*\n");
+	printf("\t*****************************************\n\n");
+	printf("\tSelecciona una Opcion\n");
+	printf("\t1) Depositar Dinero\n");
+	printf("\t2) Retirar Dinero\n");
+	printf("\t3) Salir del Cajero\n");
+	printf("\t\nOpcion Seleccionada: ");
+	scanf("%i",&opcion);
+	
+	switch(opcion){
+		case 1: printf("Cuanto dinero quiere Depositar?\n");
+		scanf("%f",&agregar);
+		saldo += agregar;
+		printf("Tu saldo actual es de %f\n",saldo);
+		break;
+
+		case 2: printf("Cuanto dinero quiere Retirar\n");
+		scanf("%f",&retirar);
+		
+		if(retirar>saldo){
+			printf("Tu saldo disponible solo es de %f\n",saldo);
+		}else{
+			saldo -= retirar;
+			printf("Tu saldo total es de %f\n",saldo);
+		}
+		break;
+
+		case 3: printf("Gracias por usar la aplicacion\nHasta la proxima\n");
+		break;
+		
+	}
+	return 0;
+}
