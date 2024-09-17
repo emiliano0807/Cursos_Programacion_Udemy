e10999e7744b5e192e63df357ea953f8d7579bd9
emiliano0807 <emilianorivfacio@gmail.com>
2024-09-16 22:18:11 -0600

    Correccion del codigo

--- Codigo34.c
+++ Codigo34.c
@@ -1,4 +1,4 @@
-/*Visualizar la tarifa de la luz segun el gasto de corriente
+/*Visualiza la tarifa de la luz segun el gasto de corriente
 electrica.
 Para un gasto menor de 1.000Kwxh
 la tarifa es 1.2, entre 1.OOO y 1.850Kwxh es 1.0 y mayor
