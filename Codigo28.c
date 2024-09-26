73373aaba8913655a0af49390a158228f17868f6
emiliano0807 <emilianorivfacio@gmail.com>
2024-09-25 20:00:52 -0600

    Codigo28

---
+++ Codigo28.c
@@ -0,0 +1,16 @@
+//  Determinar si un numero es par, impar
+
+#include <stdio.h>
+
+int main(){
+    int numero;
+    printf("Escribe un numero\n");
+    scanf("%d",&numero);
+
+    if(numero%2==0){
+        printf("El numero es par\n");
+    }else{
+        printf("El numero es impar\n");
+    }
+    return 0;
+}
