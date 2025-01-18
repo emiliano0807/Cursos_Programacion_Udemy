27522ce2027331a763f2111df5121a1b6d32dc99
emiliano0807 <emilianorivfacio@gmail.com>
2025-01-18 06:16:08 -0600

    Modificacion

--- Codigo33.c
+++ Codigo33.c
@@ -1,6 +1,6 @@
 /* Ingresar por teclado el nombre, la edad y el sexo de
 cualquier persona e
-imprima, solo si la persona es de sexo femenino 
+imprima, solo si la persona es de sexo femenino y
 mayor de edad, el nombre de la persona.*/
 
 #include <stdio.h>
