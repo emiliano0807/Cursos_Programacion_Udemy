6779f54ccca876785bc2eba8531d16013dd42547
emiliano0807 <emilianorivfacio@gmail.com>
2025-01-20 06:16:46 -0600

    Modificacion

--- Codigo33.c
+++ Codigo33.c
@@ -1,4 +1,4 @@
-/* Igresar por teclado el nombre, la edad y el sexo de
+/* Ingresar por teclado el nombre, la edad y el sexo de
 cualquier persona e
 imprima, solo si la persona es de sexo femenino y
 mayor de edad, el nombre de la persona.*/
