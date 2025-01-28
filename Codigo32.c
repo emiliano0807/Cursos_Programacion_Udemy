af71ec1814e5fb5f661d7ada83046cd2beb4ee57
emiliano0807 <emilianorivfacio@gmail.com>
2025-01-28 06:19:18 -0600

    Modificacion

--- Codigo32.c
+++ Codigo32.c
@@ -1,6 +1,5 @@
 /*Ingrese un numero y calcule e imprima su raiz cuadrada.
-Si el numero es negativo imprima el numero y un mensaje que diga tiene
-raiz imaginaria */
+Si el numero es negativo imprima el numero y un mensaje que diga tiene raiz imaginaria */
 
 #include <stdio.h>
 #include <math.h>
